16466a80f09568d0d62ee79ba456ebf0056207b7
Ragil <ragil8999@gmail.com>
2018-08-14 14:59:55 +0700

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
 import { NavBar } from "antd-mobile";
-import { Icon } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <NavBar
-                    mode="light"
-                    icon={<Link to='/'><Icon name="backward" /></Link>}
-                    onLeftClick={() => console.log("onLeftClick")}
-                    rightContent={[
-                        <Icon key="0" type="search" style={{ marginRight: "16px" }} />,
-                        <Icon key="1" name="shop" />
-
-                    ]}>
-                    NavBar
+  render() {
+    return (
+      <div>
+        <NavBar
+          mode="light"
+          icon={
+            <Link to="/">
+              <Icon name="backward" />
+            </Link>
+          }
+          onLeftClick={() => console.log("onLeftClick")}
+          rightContent={[
+            <Icon key="0" type="search" style={{ marginRight: "16px" }} />,
+            <Icon key="1" name="shop" />
+          ]}>
+          Grabfood Clone
         </NavBar>
-
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
